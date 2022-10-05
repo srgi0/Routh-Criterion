16afe805eaeae1e61a2f407cd69e434d7e66cd3a
Sergio Filho <filho.sf14@gmail.com>
2022-10-05 17:32:15 -0300

    special cases

--- routh_criterion.py
+++ routh_criterion.py
@@ -62,9 +62,9 @@ class RouthTable:
     def __init__(self, Poly):
         self.Poly = Poly
         self.routh_table = 0
-        self.sufficient_condition = 0
-        self.special_case_1 = 0
-        self.special_case_2 = 0
+        self.sufficient_condition_flag = 0
+        self.special_case_1_flag = 0
+        self.special_case_2_flag = 0
         self.routh_table_rows = 0
         self.routh_table_columns = 0
     
@@ -79,6 +79,15 @@ class RouthTable:
                     while (np.all(row_from_matrix_simplified%div == 0)):
                         row_from_matrix_simplified = row_from_matrix_simplified/div
             return row_from_matrix_simplified
+
+        def check_special_cases (self, row_from_matrix):
+            if ( self.routh_table[row_from_matrix,0] == 0 ) and \
+            not ( np.all(self.routh_table[row_from_matrix,:] == 0) ) :
+                self.special_case_1_flag = True
+                self.special_case_2_flag = False
+            else:
+                self.special_case_1_flag = False
+                self.special_case_2_flag = True
 
         self.routh_table_rows = self.Poly.grau + 1
         self.routh_table_columns = max(len(self.Poly.par_coefs), len(self.Poly.impar_coefs))
@@ -99,6 +108,13 @@ class RouthTable:
                     variant_column = self.routh_table[(row-2):2+(row-2), [column+1]]
                     square_matrix = np.concatenate((stable_column,variant_column), axis=1)
                     self.routh_table[row,column] = calc(square_matrix)
+            
+                check_special_cases(row)
+                if (self.special_case_1_flag):
+                    self.routh_table[row,0] = 1/np.inf
+                elif (self.special_case_2_flag):
+                    pass
+
                 self.routh_table[row,:] = simplify_row(self.routh_table[row,:])
 
         else:
@@ -110,26 +126,17 @@ class RouthTable:
                     variant_column = self.routh_table[(row-2):2+(row-2), [column+1]]
                     square_matrix = np.concatenate((stable_column,variant_column), axis=1)
                     self.routh_table[row,column] = calc(square_matrix)
+                
+                check_special_cases(row)
 
 
     def check_sufficient_condition (self):
-        if (np.all(self.routh_table[:,0] > 0)):
-            self.sufficient_condition = True
-        else:
-            self.sufficient_condition = False
-
-    def check_special_case_1 (self):
-        if (np.any(self.routh_table[:,0] == 0)):
-            self.special_case_1 = True
-        else:
-            self.special_case_1 = False       
-
-    def check_special_case_2 (self):
-        for row in range(self.routh_table_rows):
-            if (np.all(self.routh_table[row,:] == 0)):
-                self.special_case_2 = True
+            if (np.all(self.routh_table[:,0] > 0)):
+                self.sufficient_condition_flag = True
             else:
-                self.special_case_2 = False
+                self.sufficient_condition_flag = False
+
+    
 
 
 
@@ -139,7 +146,7 @@ poly.check_necessary_condition()
 print(f'Necessary condition is {poly.necessary_condition}')
 
 routh_table = RouthTable(poly)
-routh_table.make_table(True)
+routh_table.make_table(False)
 print(routh_table.routh_table)
 routh_table.check_sufficient_condition()
-print(f'Sufficient condition is {routh_table.sufficient_condition}')
+print(f'Sufficient condition is {routh_table.sufficient_condition_flag}')
